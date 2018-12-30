f56057dae6cb86db01c69c149fb80d973042ad0b
Sjampie <samtieman@gmail.com>
2018-12-30 19:53:12 +0100

    added more examples

---
+++ Javascript & JQuery Book/Functions, Methods & Objects/contstructorObject.js
@@ -0,0 +1,24 @@
+//blue print starts here
+function Person(firstName, lastName, age) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.age = age;
+  this.updateAge = function(){
+    return ++this.age;
+  };
+}
+//blueprint ends here
+var person01 = new Person("Sam", "Tieman", 20) //create new object using the blueprint called person01
+console.log(person01.firstName);
+
+/* The big difference between a literal object and a constructor object
+Is that a literal object have assigned properties while a contstructor use a blueprint so
+it can make more objects using the blueprint.
+*/
+
+//Adds new property
+person01.glasses = false;
+console.log(person01.glasses);
+//Delete property 
+delete person01.glasses
+console.log(person01.glasses);
--- Javascript & JQuery Book/Functions, Methods & Objects/literalObject.js
+++ Javascript & JQuery Book/Functions, Methods & Objects/literalObject.js
@@ -13,4 +13,9 @@ var hotel = {
 //Dot notation (Calling a object)
 var hotelName = hotel.name;
 var roomsFree = hotel.checkAvailability();
-console.log(hotelName, roomsFree);
+console.log(hotelName, roomsFree); //OUTPUT = Quay, 15
+//You can also make use of sqaure brackets []
+var hotelNameBR = hotel['name'];
+var roomsFreeBR = hotel['checkAvailability']();
+console.log(hotelNameBR, roomsFreeBR);
+//This form of notation is used when there is a numberr or symbol in the name of the property 
